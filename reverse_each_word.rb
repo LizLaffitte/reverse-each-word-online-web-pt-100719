0e87e99d89741b7b2f9ba0dd5810ef876cee0327
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-19 21:59:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
-def reverse_each_word(phrase)
-  new_phrase =""
-  phrase_array = phrase.split(" ")
-  phrase_array.collect do |word|
-    word.reverse!
-  end 
-  phrase_array.join(" ")
-end 
+def reverse_each_word(my_string)
+	my_array = my_string.split
+	my_array.collect do |word|
+		word.reverse!
+	end
+  my_array.join(" ")
+end
